df2f490e1da1825491df200c3f2f898368daa143
Chandrakala152 <chandrakalakotapati6@gmail.com>
2026-01-24 21:22:07 +0530

    Add logging and Error Handling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from src.sensor import Sensor
 from src.storage import FileStorage
+from src.logger import setup_logger
+import logging
 import time
 
 def main():
+    logger= setup_logger()
+    logger.info("Sensor system started")
+
     sensors = [
         Sensor("TEMP_001", "temperature", "C"),
         Sensor("HUM_001", "humidity", "%"),
@@ -15,13 +20,16 @@ def main():
         while True:
             for sensor in sensors:
                 reading = sensor.generate_reading() 
-                print("📡", reading)
+                logger.info(f"Generated reading: {reading}")
                 storage.save(reading)
 
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print("🛑 Stopped")
+        logger.warning("Sensor system stopped by user")
+
+    except Exception as e:
+        logger.error(f"System crashed: {e}")
 
 if __name__ == "__main__":
     main()
--- src/storage.py
+++ src/storage.py
@@ -1,5 +1,6 @@
 import os
 import json
+import logging
 
 class FileStorage:
     def __init__(self, filename="sensor_data.json"):
@@ -8,6 +9,10 @@ class FileStorage:
         self.filepath = os.path.join(self.data_dir, filename)
 
     def save(self, reading):
-        with open(self.filepath, "a", encoding="utf-8") as f:
-            json.dump(reading, f)
-            f.write("\n")
+        try:
+            with open(self.filepath, "a", encoding="utf-8") as f:
+              json.dump(reading, f)
+              f.write("\n")
+            logging.info(f"Saved reading from {reading['sensor_id']}")
+        except Exception as e:
+            logging.error(f"Failed to save reading: {e}")
